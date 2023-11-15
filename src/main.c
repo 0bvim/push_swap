f4ff332ac5a3a8851dd417f5fd8d3c0978cd5eec
nivicius <niviciusdev@gmail.com>
2023-11-15 00:40:48 -0300

    implemented split on argv withou validation if is only number or if number is double

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 #include "push_swap.h"
 #include <stdio.h>
 
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
 	// t_stack	*a;
 	// t_stack *b;
@@ -12,7 +12,7 @@ int main(int argc, char const *argv[])
 		return (ft_putendl_fd("invalid args\n", 2), 1);
 	else if (2 == argc)
 		argv = ft_split(argv[1], ' ');
-	for (int i = 1; i < argc; i++)
+	for (int i = 1; i <= argc - 1; i++)
 		printf("Pos %d        Value %s\n", i, argv[i]);
 	return (ft_putendl_fd("valid args\n", 1), 0);
 }
