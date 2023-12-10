8ce32729f7d79a84b8ec51ac6bd8d7c41eebc5d8
vde-frei <vde-frei@student.42sp.org.br>
2023-12-10 20:56:42 -0300

    feat: changed file name and header

--- includes/checker.h
+++ includes/checker.h
@@ -3,20 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   checker.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nivicius <nivicius@student.42.fr>          +#+  +:+       +#+        */
+/*   By: vde-frei <vde-frei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 10:59:34 by nivicius          #+#    #+#             */
-/*   Updated: 2023/11/23 23:18:47 by nivicius         ###   ########.fr       */
+/*   Updated: 2023/12/10 20:56:26 by vde-frei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CHECKER_H
 # define CHECKER_H
 
-// includes
-# include "../lib/libft/includes/libft.h"
-# include <limits.h>
-# include <stdbool.h>
-# include <stdio.h>
+/* includes */
+# include "push_swap.h"
+
 
 #endif
