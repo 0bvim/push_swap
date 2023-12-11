0b87ebbe6b4fcc5be51764e4f6bad45e3ce18870
vde-frei <vde-frei@student.42sp.org.br>
2023-12-11 06:33:02 -0300

    feat: implemented checker... I think

--- src/bonus/checker.c
+++ src/bonus/checker.c
@@ -6,19 +6,19 @@
 /*   By: vde-frei <vde-frei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 21:25:23 by vde-frei          #+#    #+#             */
-/*   Updated: 2023/12/11 01:29:09 by vde-frei         ###   ########.fr       */
+/*   Updated: 2023/12/11 06:31:38 by vde-frei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/checker.h"
 
-static void	free_checker(char **argv, t_stack *a);
+static void	mid_parse(t_stack **a, t_stack **b);
+static void	free_checker(t_stack *a);
 
 int	main(int argc, char *argv[])
 {
 	t_stack	*a;
 	t_stack	*b;
-	char	*next_line;
 	size_t	len;
 
 	a = NULL;
@@ -27,27 +27,33 @@ int	main(int argc, char *argv[])
 		exit(EXIT_SUCCESS);
 	else if (2 == argc)
 		argv = ft_split(argv[1], 0x20);
-	stack_init(&a, argv, 2 == argc);
+	stack_init(&a, argv + 1, 2 == argc);
 	len = stack_len(a);
-	next_line = get_next_line(IN);
-	while (next_line)
-	{
-		parse_command(&a, &b, next_line);
-		next_line = get_next_line(IN);
-	}
+	mid_parse(&a, &b);
 	if (stack_sorted(a) && (stack_len(a) == len))
 		ft_putstr_fd("OK\n", OUT);
 	else
 		ft_putstr_fd("KO\n", OUT);
-	free_checker(argv, a);
+	free_checker(a);
 	return (EXIT_SUCCESS);
 }
 
-static void	free_checker(char **argv, t_stack *a)
+static void	free_checker(t_stack *a)
 {
-	if (argv)
-		ft_free_split(argv);
 	while (a->prev && (a->prev != a->prev->prev))
 		a = a->prev;
 	free_list(a);
+}
+
+static void	mid_parse(t_stack **a, t_stack **b)
+{
+	char	*next_line;
+
+	next_line = get_next_line(IN);
+	while (next_line)
+	{
+		parse_command(a, b, next_line);
+		free(next_line);
+		next_line = get_next_line(IN);
+	}
 }
--- src/bonus/parser.c
+++ src/bonus/parser.c
@@ -6,7 +6,7 @@
 /*   By: vde-frei <vde-frei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/10 23:14:11 by vde-frei          #+#    #+#             */
-/*   Updated: 2023/12/11 01:32:08 by vde-frei         ###   ########.fr       */
+/*   Updated: 2023/12/11 06:24:56 by vde-frei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,23 +21,23 @@ void	parse_command(t_stack **a, t_stack **b, char *command)
 		pa(a, b, TRUE);
 	else if (!simple_comp(command, "pb\n"))
 		pb(b, a, TRUE);
-	else if (!simple_comp(command, "pb\n"))
+	else if (!simple_comp(command, "sa\n"))
 		sa(a, TRUE);
-	else if (!simple_comp(command, "pb\n"))
+	else if (!simple_comp(command, "sb\n"))
 		sb(b, TRUE);
-	else if (!simple_comp(command, "pb\n"))
+	else if (!simple_comp(command, "ss\n"))
 		ss(a, b, TRUE);
-	else if (!simple_comp(command, "pb\n"))
+	else if (!simple_comp(command, "ra\n"))
 		ra(a, TRUE);
-	else if (!simple_comp(command, "pb\n"))
+	else if (!simple_comp(command, "rb\n"))
 		rb(b, TRUE);
-	else if (!simple_comp(command, "pb\n"))
+	else if (!simple_comp(command, "rr\n"))
 		rr(a, b, TRUE);
-	else if (!simple_comp(command, "pb\n"))
+	else if (!simple_comp(command, "rra\n"))
 		rra(a, TRUE);
-	else if (!simple_comp(command, "pb\n"))
+	else if (!simple_comp(command, "rrb\n"))
 		rrb(b, TRUE);
-	else if (!simple_comp(command, "pb\n"))
+	else if (!simple_comp(command, "rrr\n"))
 		rrr(a, b, TRUE);
 	else
 		handle_error(a, b);
@@ -50,7 +50,7 @@ static int	simple_comp(char *str1, char *str2)
 		++str1;
 		++str2;
 	}
-	return (*str1 - *str2);
+	return ((unsigned char)*str1 - (unsigned char)*str2);
 }
 
 static void	handle_error(t_stack **a, t_stack **b)
